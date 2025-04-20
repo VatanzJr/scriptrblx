1c416aed048aa05979c7bee322bb6fc80138b715
VatanzJr <yusuf.vtnz@gmail.com>
2025-04-21 00:09:23 +0800

    Update rayfieldyeet.lua

--- rayfieldyeet.lua
+++ rayfieldyeet.lua
@@ -33,3 +33,5 @@ local Window = Rayfield:CreateWindow({
       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
+
+local Tab = Window:CreateTab("Main", 11708967881) -- Title, Image
