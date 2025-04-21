7f86e44077eb237b074cb80b7c5be64592879efa
VatanzJr <yusuf.vtnz@gmail.com>
2025-04-21 15:37:56 +0800

    Update rayfieldyeet.lua

--- rayfieldyeet.lua
+++ rayfieldyeet.lua
@@ -99,7 +99,7 @@ ToolsTab:CreateButton({
 ToolsTab:CreateButton({
     Name = "Simple Spy",
     Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpyBeta.lua", true))()
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpyBeta.lua"))()
     end,
 })
 
