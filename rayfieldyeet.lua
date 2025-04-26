21439a36e58872fffcf18984a0edd0fb1d856ffc
VatanzJr <yusuf.vtnz@gmail.com>
2025-04-26 21:45:07 +0800

    Update rayfieldyeet.lua

--- rayfieldyeet.lua
+++ rayfieldyeet.lua
@@ -6,7 +6,7 @@ local RunService = game:GetService("RunService")
 
 local Window = Rayfield:CreateWindow({
     Name = "Vatanz Hub",
-    LoadingTitle = "Multi-Feature Hub oke2",
+    LoadingTitle = "Multi-Feature Hub oke232",
     LoadingSubtitle = "by Vatanz",
     ConfigurationSaving = { Enabled = true, FileName = "VatanzHub" }
 })
